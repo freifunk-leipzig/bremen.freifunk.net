87bd27e747cc5a3a877708281388eb744a91ccae
mokkin <markus@haybach.com>
2020-10-29 21:52:32 +0100

    gebaute site ins git geschoben

---
+++ _site/assets/firmware.js
@@ -0,0 +1,128 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var
+    firmware_base_url = 'http://firmware.leipzig.freifunk.net:8055/srv2/gluon/1.2.3/output/images/',
+    branch = 'stable',
+    vendors = {
+      "tp-link": "TP-LINK",
+      "ubiquiti": "Ubiquiti",
+      "d-link": "D-Link",
+      "liksys": "Linksys",
+      "buffalo": "Buffalo",
+      "netgear": "Netgear",
+      "allnet": "Allnet",
+      "gl": "GL Innovations",
+      "wd": "Western Digital",
+      "onion": "Onion",
+      "openmesh": "OpenMesh",
+      "alfa-network": "ALFA Network",
+      "meraki": "Cisco Meraki",
+      "8devices": "8devices",
+      "raspberry": "Raspberry",
+      "x86": "PC",
+    },
+    no_factory = ["8devices", "meraki", "unifi-ac", "gl-ar150", "allnet", "wzr-600dhp"],
+    xhr = window.XMLHttpRequest? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'),
+    searchbox = document.getElementById('model-search');
+
+  xhr.open('GET', firmware_base_url + branch + '/sysupgrade/' + branch + '.manifest');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState <= 3 || xhr.status != 200)
+      return;
+    var models = [],
+      lines = xhr.responseText.split("\n");
+    lines.sort();
+    lines.forEach(function(line) {
+      var lineparts = line.split(" "),
+        model = lineparts[0],
+        filename = lineparts[lineparts.length-1];
+      if (lineparts.length != 4)
+        return;
+      for (vendor in vendors) {
+        if (model.indexOf(vendor) == 0) {
+          var name = new Array(),
+            model = model.substr(vendor.length + 1);
+          model.split('-').forEach(function(part) {
+            if (part.match(/^v[\d\.]+$/))
+              name.push(part);
+            else if (part == "unifi")
+              name.push("UniFi");
+            else if (part.match(/\d/) || part.length <= 3)
+              name.push(part.toUpperCase());
+            else
+              name.push(part[0].toUpperCase() + part.substr(1));
+          });
+
+          name = name.join(" ").replace('N ND ', 'N/ND ');
+
+          if (models.length == 0 || models[models.length-1].file != filename)
+            models.push({
+              vendor: vendor,
+              model: name,
+              id: model,
+              file: filename,
+            });
+          break;
+        }
+      }
+    });
+    var model_div = document.querySelector('#models');
+    var model_template = document.getElementById('model-template').content;
+    models.forEach(function(model) {
+      var tmpl = document.importNode(model_template, true),
+        container = tmpl.querySelector('.model'),
+        has_factory = true;
+      container.style.backgroundImage = "url('/images/routers/" + model.vendor + "-" + model.id + ".svg')";
+      container.dataset.searchterms =
+        vendors[model.vendor].toLowerCase().replace(/[^a-z0-9]/g, '')
+        + " " +
+        model.model.toLowerCase().replace(/[^a-z0-9]/g, '');
+      container.querySelector('.name').textContent = vendors[model.vendor] + " " + model.model;
+      container.querySelector('.update a').href = firmware_base_url + branch + '/sysupgrade/' + model.file;
+      no_factory.forEach(function(pattern) {
+        if (model.file.indexOf(pattern) != -1)
+          has_factory = false;
+      });
+      if (has_factory)
+        container.querySelector('.factory a').href =
+          firmware_base_url + branch + '/factory/' +
+          model.file.replace('-sysupgrade', '').replace(/(-wndr.*)\.(tar|bin)$/, "$1.img");
+      else {
+        tmpl.querySelector('.factory').remove();
+        container.querySelector('.update a').textContent +=' / Erstinstallation';
+      }
+      model_div.appendChild(tmpl);
+    });
+  }
+  xhr.send();
+
+  searchbox.addEventListener("input", function(ev) {
+    var searchterm = this.value.toLowerCase();
+    var searchbox = document.getElementById('model-search');
+    if (searchterm.replace(' ', '').length < 3) {
+      searchbox.setCustomValidity('');
+      return;
+    }
+    var searchterms = searchterm.split(' ');
+    var any = false;
+    [].forEach.call(document.querySelectorAll('#models .model'), function(a) {
+      var show = true;
+      searchterms.forEach(function(searchterm) {
+        if (a.dataset.searchterms.indexOf(searchterm.replace(/[^a-z0-9]/g, '')) == -1)
+          show = false;
+      });
+      any |= show;
+      a.style.display = show? "block" : "";
+    });
+    searchbox.setCustomValidity(any? "" : "Keine Modelle gefunden!");
+  });
+  searchbox.value = "";
+  searchbox.focus();
+
+  document.getElementById('show-all').addEventListener("click", function(ev) {
+    [].forEach.call(document.querySelectorAll('#models .model'), function(a) {
+      a.style.display = "block";
+    });
+  });
+});
+
+// vim: syntax=javascript sw=2 ts=2 sts=2 et
--- kontakt.md
+++ kontakt.md
@@ -7,13 +7,12 @@ Kontakt
 
 Bei Fragen zum Freifunk-Bremen-Projekt freuen wir uns über Kontaktaufnahmen:
 
-* per E-Mail: [info@bremen.freifunk.net](mailto:info@bremen.freifunk.net)
+* per E-Mail: [info@leipzig.freifunk.net](mailto:info@leipzig.freifunk.net)
 * im Chat:
   * per Browser: <a href="https://webirc.hackint.org/#ircs://irc.hackint.org/#ffhb?nick=Gast_?" target="_blank">Webchat</a>
-  * IRC: [#ffhb im Hackint](irc://irc.hackint.org/ffhb)
   * XMPP/Jabber: [#ffhb@irc.hackint.org](xmpp:#ffhb@irc.hackint.org)
-* auf Twitter: [@FreifunkHB](https://twitter.com/FreifunkHB)
-* auf Facebook: [FreifunkBremen](https://www.facebook.com/FreifunkBremen)
+* auf Twitter: [@Freifunk_L](https://twitter.com/freifunk_l)
+* auf Facebook: [FreifunkLeipzig](https://de-de.facebook.com/FreifunkLeipzig)
 
 ## Reaktionszeit
 Wir geben uns Mühe, innerhalb eines **Tages** (im Chat in einer **Stunde**) zu antworten; dies ist jedoch nicht immer möglich. Wir bitten deshalb um ein wenig Geduld, lasst euch Chatfenster länger geöffnet und erwähnt einen der Anwesenden mit Namen, wartet ein paar Tage nach einer Mail und schickt dann gerne eine Erinnerungsmail. 
@@ -24,8 +23,8 @@ Für Fragen zu spezifischen Themen, gibt es eine  Wiki-Seite für [Ansprechpartn
 Ansonsten empfiehlt es sich immer einen Blick in unsere [FAQs](faq.html) und ins [Wiki](http://wiki.bremen.freifunk.net/) zu werfen. Viele Fragen und Themen sind dort bereits beantwortet und erklärt.
 
 ## Treffen
-Wir treffen uns außerdem zweimal im Monat, jeweils am 1. und 3. Freitag ab 19 Uhr im [Hackerspace
-Bremen](https://www.hackerspace-bremen.de/anfahrt/). Schau doch mal vorbei, wir
+Wir treffen uns außerdem zweimal im Monat, jeweils am 1. und 3. Donnerstag ab 20 Uhr im Leipziger Hackerspace [Dezentrale]
+](https://dezentrale.space/). Schau doch mal vorbei, wir
 freuen uns immer über neue Gesichter!
 
 ## Mailingliste
